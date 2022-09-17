cc903bc753f4d4318978a35940c75e1e5c740ee4
slawo <slawekpettke@gmail.com>
2022-09-17 18:51:06 +0200

    matchery - hamcrest assertJ

--- TDD/src/test/java/pl/slaw/account/AccountTest.java
+++ TDD/src/test/java/pl/slaw/account/AccountTest.java
@@ -2,6 +2,11 @@ package pl.slaw.account;
 
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -11,11 +16,12 @@ class AccountTest {
 
     @Test
     void newAccountShouldntBeActiveAfterCreate() { // test zawsze jest voidem
-        //obiekt klasy testowanej
         Account newAccount = new Account();
-        // assercja -dajemy to co chcemy sprawdzic czyli
-        // czy newAccount jest active
         assertFalse(newAccount.isActive());
+        //tutaj podajemy odwrotnie, tzn na poczatku actual a potem expected
+        //przykład z matcherami
+        assertThat(newAccount.isActive(),equalTo(false));
+        assertThat(newAccount.isActive(),is(false));
     }
 
     @Test
@@ -23,47 +29,41 @@ class AccountTest {
         //given
         Account newAccount = new Account();
         //when
-        //sprawdzenie czy po aktywacji zmieni sie stan
         newAccount.activate();
         //then
         assertTrue(newAccount.isActive());
+
+        //wersja z matcherami
+        assertThat(newAccount.isActive(), equalTo(true));
+        assertThat(newAccount.isActive(), is(true));
     }
 
-    //chcemy zeby nowoutworzone account mialo adres null na poczatku
-    //sprawdzimy czy tak bedzie
+
     @Test
     void newlyCreatedAccountShouldNotHaveDefaultDeliveryAddressSet(){
         //given
         Account account = new Account();
-
-        //when - tworzymy nowy adres do ktorego przypisujemy adres z
-        //nowo utworzonego obiektu account
+        //when
         Address address = account.getDefaultDeliveryAddress();
-
         //then
         assertNull(address);
-
+        //wersja z matcherem
+        assertThat(address, nullValue());
     }
 
-    //sprawdzenie czy obiekt nie jest nullem
     @Test
     void defaultDeliveryAddressShouldNotBeNullAfterBeingSet(){
         //given
         Address address = new Address("Długa", "67c");
         Account account = new Account();
         account.setDefaultDeliveryAddress(address);
-
         //when
         Address defaultAddress = account.getDefaultDeliveryAddress();
-
         //then
         assertNotNull(defaultAddress);
-
+        //wersja z matcherem
+        assertThat(defaultAddress, notNullValue());
+        assertThat(defaultAddress, is(notNullValue()));
     }
-
-
-
-
-
 
 }
