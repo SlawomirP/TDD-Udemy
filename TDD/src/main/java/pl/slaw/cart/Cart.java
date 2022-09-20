7f647f88e88d0111efa5695038a54016543e4949
slawo <slawekpettke@gmail.com>
2022-09-20 14:06:42 +0200

    testowanie wydajności

---
+++ TDD/src/main/java/pl/slaw/cart/Cart.java
@@ -0,0 +1,34 @@
+package pl.slaw.cart;
+
+import pl.slaw.meal.Meal;
+import pl.slaw.order.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
+    private List<Order> ordersCart = new ArrayList<>();
+
+    void addOrderToCart(Order order){
+        this.ordersCart.add(order);
+    }
+
+    void clearCart(){
+        this.ordersCart.clear();
+    }
+
+    //utworzenie symulatora zamówień
+    void simulateLargeOrder(){
+        for (int i = 0; i < 1000; i++) {
+            Meal meal = new Meal(i%10, "Hamburger no " + i);
+            Order order = new Order();
+            order.addMealToOrder(meal);
+            addOrderToCart(order);
+        }
+        System.out.println("Cart size: " + ordersCart.size());
+        clearCart();
+
+    }
+
+}
---
+++ TDD/src/test/java/pl/slaw/cart/CartTest.java
@@ -0,0 +1,12 @@
+package pl.slaw.cart;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    @Test
+    void simulateLargeOrder() {
+    }
+}
