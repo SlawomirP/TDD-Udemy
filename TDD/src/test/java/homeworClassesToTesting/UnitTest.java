c0fbf245194b3fee26eb64b232c4ad385721b93c
slawo <slawekpettke@gmail.com>
2022-09-27 14:29:06 +0200

    homework writing tests Unit class

--- TDD/src/test/java/homeworClassesToTesting/UnitTest.java
+++ TDD/src/test/java/homeworClassesToTesting/UnitTest.java
@@ -29,21 +29,83 @@ class UnitTest {
         Coordinates result = unit.move(5, 5);
 
         //then
-        assertAll(
-                () -> assertThat(result.getX(), equalTo(6)),
-                () -> assertThat(result.getY(), equalTo(6))
-        );
+        assertAll(() -> assertThat(result.getX(), equalTo(6)), () -> assertThat(result.getY(), equalTo(6)));
     }
 
     @Test
     void loadCargoMethodShouldTwhowsExceptionAfterExceedMaxCargoWeight() {
         //given
-        Unit unit = new Unit(new Coordinates(1,1), 5, 10);
-        Cargo grain = new Cargo("Grain", 15);
+        Unit unit = new Unit(new Coordinates(1, 1), 5, 10);
+        Cargo grain = new Cargo("Grain", 11);
+
+        //when
+        //then
+        assertThrows(IllegalStateException.class, () -> unit.loadCargo(grain));
+    }
+
+    @Test
+    void loadCargoMethodShouldAddCargoWhenLimitIsNotExceed() {
+        //given
+        Unit unit = new Unit(new Coordinates(1, 1), 5, 10);
+        Cargo grain = new Cargo("Grain", 5);
 
         //when
         unit.loadCargo(grain);
 
+        //then
+        assertThat(unit.getLoad(), equalTo(5));
     }
 
+    @Test
+    void loadCargoMethodShouldAddFewCargosWhenLimitIsNotExceed() {
+        //given
+        Unit unit = new Unit(new Coordinates(1, 1), 5, 20);
+        Cargo grain = new Cargo("Grain", 5);
+        Cargo sand = new Cargo("Sand", 5);
+        Cargo liquidGas = new Cargo("Liquid gas", 5);
+
+        //when
+        unit.loadCargo(grain);
+        unit.loadCargo(sand);
+        unit.loadCargo(liquidGas);
+
+        //then
+        assertThat(unit.getLoad(), equalTo(15));
+    }
+
+    @Test
+    void unloadCargoMethodShouldRemoveCargoGivenInParameterFromTheHold(){
+        //given
+        Unit unit = new Unit(new Coordinates(1, 1), 5, 10);
+        Cargo grain = new Cargo("Grain", 2);
+        Cargo sand = new Cargo("Sand", 3);
+        Cargo liquidGas = new Cargo("Liquid gas", 5);
+
+        //when
+        unit.loadCargo(grain);
+        unit.loadCargo(sand);
+        unit.loadCargo(liquidGas);
+        unit.unloadCargo(sand);
+
+        //then
+        assertThat(unit.getLoad(), equalTo(7));
+    }
+
+    @Test
+    void unloadAllCargoMethodShouldClearTheHold(){
+        //given
+        Unit unit = new Unit(new Coordinates(1, 1), 5, 10);
+        Cargo grain = new Cargo("Grain", 2);
+        Cargo sand = new Cargo("Sand", 3);
+        Cargo liquidGas = new Cargo("Liquid gas", 5);
+
+        //when
+        unit.loadCargo(grain);
+        unit.loadCargo(sand);
+        unit.loadCargo(liquidGas);
+        unit.unloadAllCargo();
+
+        //then
+        assertThat(unit.getLoad(), equalTo(0));
+    }
 }
