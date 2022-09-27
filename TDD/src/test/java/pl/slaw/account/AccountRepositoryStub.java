007b074ede6a9426aaf4e58d0453b02ed496bc6a
slawo <slawekpettke@gmail.com>
2022-09-27 19:57:49 +0200

    MOCKITO - stub

---
+++ TDD/src/test/java/pl/slaw/account/AccountRepositoryStub.java
@@ -0,0 +1,21 @@
+package pl.slaw.account;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AccountRepositoryStub implements AccountRepository {
+
+    //tutaj bedzie metoda zwracajaca przykladowe dane
+    @Override
+    public List<Account> getAllAccounts() {
+        Address address1 = new Address("Długa", "2E/2");
+        Account account1 = new Account(address1);
+
+        Account account2 = new Account();
+
+        Address address2 = new Address("Krótka", "3a");
+        Account account3 = new Account(address2);
+
+        return Arrays.asList(account1, account2, account3);
+    }
+}
--- TDD/src/test/java/pl/slaw/account/AccountServiceTest.java
+++ TDD/src/test/java/pl/slaw/account/AccountServiceTest.java
@@ -2,11 +2,27 @@ package pl.slaw.account;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountServiceTest {
 
     @Test
     void getAllActiveAccounts() {
+        //given - w tym przykladzie w konstruktorze potrzbujemy metody
+            //ktora zwróci nam dane - stworzymy taka metoda i ona zwróci nam
+            // przykladowe dane ktorych uzyjemy w tym teście
+           // stuba tworzymy w paczce z testami, potem wrzucamy tu
+        AccountRepository accountRepositoryStub = new AccountRepositoryStub(); //instancja interfejsu
+        AccountService accountService = new AccountService(accountRepositoryStub);
+
+        //when
+        List<Account> accountList = accountService.getAllActiveAccounts();
+
+        //then
+        assertThat(accountList, hasSize(2));
     }
 }
